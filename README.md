9110166cf92b4be06fc5bc99fe34a71632975a18
shkiran4 <shkiran4@in.ibm.com>
2016-08-22 13:07:22 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mqtt_arduino_registered_to_IBMBluemix
+
+#####  This is for registering the Arduino to the IBM Bluemix.
+
+            https://github.com/ibm-messaging/iot-arduino
+        There are two ways of going about this.
+        1. Quickstart
+        2. Registered.
+        
+        Create a device, and make these changes
+
+    Modify the AUTHTOKEN in the sketch code
+    Modify the MS_PROXY, in the sketch code, by providing the values in the following format "uguhsp.messaging.internetofthings.ibmcloud.com", by replacing "uguhsp" with your organization
+    Modify the CLIENT_ID, in the sketch code, by providing the values in the following format "d:uguhsp:iotsample-arduino:00aabbccde03", by replacing "aabbccde03" with the MAC Address and "uguhsp" with the organization, and the "iotsample-arduino" with the devicetype
---
+++ registered.ino
@@ -0,0 +1,167 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <IPStack.h>
+#include <Countdown.h>
+#include <MQTTClient.h>
+
+#define MQTT_MAX_PACKET_SIZE 100
+#define SIZE 100
+
+#define MQTT_PORT 1883
+
+#define PUBLISH_TOPIC "iot-2/evt/status/fmt/json"
+#define SUBSCRIBE_TOPIC "iot-2/cmd/+/fmt/json"
+#define AUTHMETHOD "use-token-auth"
+
+int ledPin = 13;
+
+#define CLIENT_ID "d:8ubmht:Arduino:00FFBBCCDE02"
+#define MS_PROXY "8ubmht.messaging.internetofthings.ibmcloud.com"
+#define AUTHTOKEN "iyI8aVbyYLJEy_4xRZ"
+// Update these with values suitable for your network.
+byte mac[] = { 0x00, 0xFF, 0xBB, 0xCC, 0xDE, 0x02 };
+
+//For Arduino Yun, instantiate a YunClient and use the instance to declare
+//an IPStack ipstack(c) instead of EthernetStack with c being the YunClient
+// e.g. YunClient c;
+// IPStack ipstack(c);
+EthernetClient c; // replace by a YunClient if running on a Yun
+IPStack ipstack(c);
+
+MQTT::Client<IPStack, Countdown, 100, 1> client = MQTT::Client<IPStack, Countdown, 100, 1>(ipstack);
+
+void messageArrived(MQTT::MessageData& md);
+
+String deviceEvent;
+
+void setup() {
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  pinMode(ledPin, OUTPUT); 
+  delay(1000);
+}
+
+void loop() {
+  int rc = -1;
+  if (!client.isConnected()) {
+    Serial.println("Connecting using Registered mode ");
+    Serial.print("\tto Client Id : ");
+    Serial.println(CLIENT_ID);
+    Serial.print("\tto MQTT Broker : ");
+    Serial.println(MS_PROXY);
+    Serial.print("\ton topic : ");
+    Serial.println(PUBLISH_TOPIC);
+    while (rc != 0) {
+      rc = ipstack.connect(MS_PROXY, MQTT_PORT);
+    }
+    Serial.println ("Connected. ");
+    MQTTPacket_connectData options = MQTTPacket_connectData_initializer;
+    options.MQTTVersion = 3;
+    options.clientID.cstring = CLIENT_ID;
+    options.username.cstring = AUTHMETHOD;
+    options.password.cstring = AUTHTOKEN;
+    options.keepAliveInterval = 10;
+    rc = -1;
+    Serial.println ("Before the connect ");
+    while ((rc = client.connect(options)) != 0)
+      ;
+    //unsubscribe the topic, if it had subscribed it before.
+ 
+    client.unsubscribe(SUBSCRIBE_TOPIC);
+    //Try to subscribe for commands
+    if ((rc = client.subscribe(SUBSCRIBE_TOPIC, MQTT::QOS0, messageArrived)) != 0) {
+            Serial.print("Subscribe failed with return code : ");
+            Serial.println(rc);
+    } else {
+          Serial.println("Subscribed\n");
+    }
+    Serial.println("Subscription tried......");
+    Serial.println("Connected successfully\n");
+    Serial.println("Temperature(in C)\tDevice Event (JSON)");
+    Serial.println("____________________________________________________________________________");
+  }
+
+  MQTT::Message message;
+  message.qos = MQTT::QOS0; 
+  message.retained = false;
+
+  char json[56] = "{\"d\":{\"myName\":\"Arduino Uno\",\"temperature\":";
+//  char buffer[10];
+  float tempValue = getTemp();
+  dtostrf(tempValue,1,2, &json[43]);
+//  dtostrf(getTemp(),1,2, buffer);
+
+  json[48] = '}';
+  json[49] = '}';
+  json[50] = '\0';
+  Serial.print("\t");
+  Serial.print(tempValue);
+  Serial.print("\t\t");
+  Serial.println(json);
+  message.payload = json; 
+  message.payloadlen = strlen(json);
+
+  rc = client.publish(PUBLISH_TOPIC, message);
+  if (rc != 0) {
+    Serial.print("Message publish failed with return code : ");
+    Serial.println(rc);
+  }
+  client.yield(1000);
+}
+
+void messageArrived(MQTT::MessageData& md) {
+  Serial.print("\nMessage Received\t");
+    MQTT::Message &message = md.message;
+    int topicLen = strlen(md.topicName.lenstring.data) + 1;
+
+    char * topic = md.topicName.lenstring.data;
+    topic[topicLen] = '\0';
+    
+    int payloadLen = message.payloadlen + 1;
+
+    char * payload = (char*)message.payload;
+    payload[payloadLen] = '\0';
+    
+    String topicStr = topic;
+    String payloadStr = payload;
+    
+    //Command topic: iot-2/cmd/blink/fmt/json
+
+    if(strstr(topic, "/cmd/blink") != NULL) {
+      Serial.print("Command IS Supported : ");
+      Serial.print(payload);
+      Serial.println("\t... Blink code removed !..\n");
+     
+      //Blink
+
+    } else {
+      Serial.println("Command Not Supported:");            
+    }
+}
+
+/*
+This function is reproduced as is from Arduino site => http://playground.arduino.cc/Main/InternalTemperatureSensor
+*/
+double getTemp(void) {
+  unsigned int wADC;
+  double t;
+
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+
+  // The returned temperature is in degrees Celcius.
+  return (t);
+}
